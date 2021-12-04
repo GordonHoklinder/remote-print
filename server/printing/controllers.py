6058c57c755791b57a173fd8dd97861b96cdf34c
GordonHoklinder <ondra.sladky@gmail.com>
2021-12-04 10:53:09 +0100

    minor improvements

--- server/printing/controllers.py
+++ server/printing/controllers.py
@@ -7,7 +7,9 @@ def save_file(file) -> str:
     return default_storage.save(filename, file)
 
 def add_file_path(file_path):
-    pass
+    tsv_path = 'in-queue.tsv'
+    with default_storage.open(tsv_path, 'a') as f:
+        f.write(file_path + '\n')
 
 def store_file(file):
     file_path = save_file(file)
