9dd725fae57e10349a7b800b3ca39aee48b964bf
GordonHoklinder <ondra.sladky@gmail.com>
2021-12-12 16:47:40 +0000

    added logging

--- server/printing/controllers.py
+++ server/printing/controllers.py
@@ -1,16 +1,24 @@
 from django.core.files.storage import default_storage
 import datetime
 
+GCODE_FOLDER_PREFIX = '/gcode-files/'
+TSV_PATH = GCODE_FOLDER_PREFIX + 'in-queue.tsv'
+LOG_PATH = 'logs/runtime-log.txt'
+
 def save_file(file) -> str:
     time_stamp = '-' + datetime.datetime.now().strftime('%d-%m-%y-%H-%M') + '.'
-    filename = time_stamp.join(file.name.rsplit('.', 1))
-    return default_storage.save(filename, file)
+    filepath = GCODE_FOLDER_PREFIX  +  time_stamp.join(file.name.rsplit('.', 1))
+    return default_storage.save(filepath, file)
 
 def add_file_path(file_path):
-    tsv_path = 'in-queue.tsv'
-    with default_storage.open(tsv_path, 'a') as f:
+    with default_storage.open(TSV_PATH, 'a') as f:
         f.write(file_path + '\n')
 
 def store_file(file):
     file_path = save_file(file)
     add_file_path(file_path)
+
+def log(message):
+    with default_storage.open(LOG_PATH, 'a') as f:
+        f.write(message + '\n')
+    
--- server/printing/views.py
+++ server/printing/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
 from .forms import SubmissionForm
-from .controllers import store_file
+from .controllers import store_file, log
 
 def submit_gcode(request):
     if request.method == 'POST':
         print()
         form = SubmissionForm(request.POST, request.FILES)
-        if form.is_valid():
+        # Temporary token.
+        # TODO: enhance security
+        log(str(form['token'].field))
+        log(str(form))
+        if form.is_valid() and form['token'].field == 'uKh+1U1j%s"Zt]k,':
             store_file(request.FILES['gcode'])
     else:
         form = SubmissionForm()
