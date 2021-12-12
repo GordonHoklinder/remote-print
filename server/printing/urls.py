d8228b58668d94d8d1e0fa0a30c31a3445f22733
GordonHoklinder <ondra.sladky@gmail.com>
2021-12-12 19:47:37 +0100

    authentication

--- server/printing/urls.py
+++ server/printing/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('print/', submit_gcode, name='print'),
+    path('login/', login_request, name='login'),
 ]
--- server/printing/views.py
+++ server/printing/views.py
@@ -1,20 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
 from .forms import SubmissionForm
 from .controllers import store_file, log
 
+@login_required(login_url='/login/')
 def submit_gcode(request):
     if request.method == 'POST':
-        print()
         form = SubmissionForm(request.POST, request.FILES)
-        # Temporary token.
-        # TODO: enhance security
-        if form.is_valid() and form['token'].value() == 'uKh+1U1js"Zt]k,':
+        if form.is_valid():
             store_file(request.FILES['gcode'])
     else:
         form = SubmissionForm()
 
-    content = {
-        'form': form,
-    }
-    return render(request, 'submit-gcode.html', content)
+    content = {'form': form,}
+    return render(request, 'form.html', content)
+
+
+def login_request(request):
+    next_page = request.GET.get('next')
+    print(next_page)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+            print(next_page)
+            if next_page is not None:
+                return redirect(next_page)
+            else:
+                return redirect('/')
+    else:
+        form = AuthenticationForm()
+
+    content = {'form': form,}
+    return render(request, 'form.html', content)
+
